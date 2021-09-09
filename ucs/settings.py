f59f28f73fd684d623489b7de75401ecfb6ae88b
Nikhil Dewoolkar <dewoolkarnikhil29@gmail.com>
2021-09-09 13:25:47 +0530

    Update settings.py

--- ucs/settings.py
+++ ucs/settings.py
@@ -154,5 +154,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'reverse.classroom@spit.ac.in'
-EMAIL_HOST_PASSWORD = 'reverse@2020'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
